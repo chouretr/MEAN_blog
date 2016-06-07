2cc2f7eca01a09a36be2be77c4c171abbfcd0c76
Aurelien <aurelien@23prod.com>
2016-06-07 02:05:15 +0200

    Ajout de passport authentification sur api (login et register)

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,47 @@
 var express = require('express');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var jwt = require('express-jwt');
 var router = express.Router();
+var User = mongoose.model('User');
+var auth = jwt({secret: 'SECRET', userproperty: 'payload'});
+
+// Creer un utilisateur
+router.post('/register', function(req, res, next) {
+    if(!req.body.email || !req.body.password){
+        return res.status(400).json({message: 'Veuillez remplir tous les champs'});
+    }
+
+    var user = new User();
+
+    user.email = req.body.email;
+
+    user.setPassword(req.body.password);
+
+    user.save(function (err) {
+        if(err){ return next(err); }
+
+        return res.json({token: user.generateJWT()})
+    });
+});
+
+router.post('/login', function (req, res, next) {
+    if(!req.body.email || !req.body.password){
+        return res.status(400).json({message: 'Veuillez remplir tous les champs'});
+    }
+    passport.authenticate('local', function(err, user, info){
+        if(err){ return next(err); }
+        if(user){
+            return res.json({token: user.generateJWT()});
+        } else {
+            return res.status(401).json(info);
+        }
+    })(req, res, next);
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,12 +2,14 @@ var express = require('express');
 var app = express();                            // créé notre application
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var passport = require('passport');
 var morgan = require('morgan');             
 var Article = require('./models/article.js');
 var methodOverride = require('method-override'); // simule des DELETE et PUT
 
-// configuration de notre application 
+// configuration de notre application
 app.use(express.static(__dirname + '/public'));                 // emplacement des fichiers statiques
+app.use(passport.initialize());
 app.use(morgan('dev'));                                         // log pour chaque requete sur la console
 app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
 app.use(bodyParser.json());                                     // parse application/json
@@ -18,10 +20,14 @@ app.use(methodOverride());
 var port = process.env.PORT || 8080;
 
 require('./models/article.js'); // on charge le modele de l'article
-require('./models/comments.js'); // on charge le modele de 
+require('./models/comments.js'); // on charge le modele de
+require('./models/users'); // model users avec les méthodes d'authentification
+require('./config/passport'); // configuration de passport
 
 var routes = require('./controllers/article');
+var routes2 = require('./controllers/index');
 app.use('/', routes);
+app.use('/', routes2);
 
 // CONNECTION A LA DB & DEMARAGE DU SERVER
 mongoose.connect('mongodb://admin:password@ds011291.mlab.com:11291/mean_blog', function(err, database) 
